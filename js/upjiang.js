858c229e79a8d2c458655e39aea7e51f1509e281
乐特 <lete@lete114.top>
2020-12-19 16:45:25 +0800

    Emoji

--- js/upjiang.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Lete on 2020/6/19.
- */
-
-/*添加图片top*/
-var top_up = "<img id="upj" class="upj" src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
